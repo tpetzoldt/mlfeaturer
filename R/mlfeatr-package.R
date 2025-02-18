3a50d0bd1ffc0a012edb3e2f6473d6d54981b82c
Thomas Petzoldt <thomas.petzoldt@tu-dresden.de>
2025-02-18 10:31:10 +0100

    separate class definitions

---
+++ R/mlfeatr-package.R
@@ -0,0 +1,5 @@
+#' Machine Learning and Feature Engineering for Regression Models
+#'
+#' This package provides preprocessing tools using S4 classes for data transformation, scaling, and inverse transformation.
+#'
+"_PACKAGE"
